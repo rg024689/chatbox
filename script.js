f9afb989efff7bc5f6496b15a655e29409ed006a
rg024689 <71029973+rg024689@users.noreply.github.com>
2020-12-18 15:10:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
-
     const msgerForm = get(".msger-inputarea");
     const msgerInput = get(".msger-input");
     const msgerChat = get(".msger-chat");
-    
+
     const BOT_MSGS = [
-    "Hi, how are you?",
-    "Ohh... I can't understand what you trying to say. Sorry!",
-    "I like to play games... But I don't know how to play!",
-    "Sorry if my answers are not relevant. :))",
-    "I feel sleepy! :("];
-    
-    
+        "Hi, how are you?",
+        "Ohh... I can't understand what you trying to say. Sorry!",
+        "I like to do coding... But I don't know how to code!",
+        "Sorry if my answers are not relevant. :))",
+        "I feel sleepy! :("
+    ];
+
+
     // Icons made by Freepik from www.flaticon.com
     const BOT_IMG = "https://image.flaticon.com/icons/svg/327/327779.svg";
     const PERSON_IMG = "https://image.flaticon.com/icons/svg/145/145867.svg";
-    const BOT_NAME = "Computer";
-    const PERSON_NAME = "Filter Coding";
-    
+    const BOT_NAME = "Ronak";
+    const PERSON_NAME = "User";
+
     msgerForm.addEventListener("submit", event => {
-      event.preventDefault();
-    
-      const msgText = msgerInput.value;
-      if (!msgText) return;
-    
-      appendMessage(PERSON_NAME, PERSON_IMG, "right", msgText);
-      msgerInput.value = "";
-    
-      botResponse();
+        event.preventDefault();
+
+        const msgText = msgerInput.value;
+        if (!msgText) return;
+
+        appendMessage(PERSON_NAME, PERSON_IMG, "right", msgText);
+        msgerInput.value = "";
+
+        botResponse();
     });
-    
+
     function appendMessage(name, img, side, text) {
-      //   Simple solution for small apps
-      const msgHTML = `
+        //   Simple solution for small apps
+        const msgHTML = `
         <div class="msg ${side}-msg">
           <div class="msg-img" style="background-image: url(${img})"></div>
     
@@ -45,33 +45,33 @@
           </div>
         </div>
       `;
-    
-      msgerChat.insertAdjacentHTML("beforeend", msgHTML);
-      msgerChat.scrollTop += 500;
+
+        msgerChat.insertAdjacentHTML("beforeend", msgHTML);
+        msgerChat.scrollTop += 500;
     }
-    
+
     function botResponse() {
-      const r = random(0, BOT_MSGS.length - 1);
-      const msgText = BOT_MSGS[r];
-      const delay = msgText.split(" ").length * 100;
-    
-      setTimeout(() => {
-        appendMessage(BOT_NAME, BOT_IMG, "left", msgText);
-      }, delay);
+        const r = random(0, BOT_MSGS.length - 1);
+        const msgText = BOT_MSGS[r];
+        const delay = msgText.split(" ").length * 100;
+
+        setTimeout(() => {
+            appendMessage(BOT_NAME, BOT_IMG, "left", msgText);
+        }, delay);
     }
-    
+
     // Utils
     function get(selector, root = document) {
-      return root.querySelector(selector);
+        return root.querySelector(selector);
     }
-    
+
     function formatDate(date) {
-      const h = "0" + date.getHours();
-      const m = "0" + date.getMinutes();
-    
-      return `${h.slice(-2)}:${m.slice(-2)}`;
+        const h = "0" + date.getHours();
+        const m = "0" + date.getMinutes();
+
+        return `${h.slice(-2)}:${m.slice(-2)}`;
     }
-    
+
     function random(min, max) {
-      return Math.floor(Math.random() * (max - min) + min);
+        return Math.floor(Math.random() * (max - min) + min);
     }
